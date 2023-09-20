561afb20ceee9b4738c44e3cd203504b863325cd
MilaMichailova <kosarevitch.mila2010@gmail.com>
2023-09-20 22:15:55 +0300

    Итоговая функциональность по пикеру со временем.

--- src/app/components/date-time-picker/date-time-picker.component.ts
+++ src/app/components/date-time-picker/date-time-picker.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbDate, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTime } from '@ng-bootstrap/ng-bootstrap/timepicker/ngb-time';
 import * as moment from 'moment';
 import { Moment } from 'moment';
 
@@ -12,23 +14,33 @@ export class DateTimePickerComponent implements OnInit {
   @Input() date: Moment = moment();
   @Output() dateChange: EventEmitter<Moment> = new EventEmitter<Moment>();
 
-  form = new FormGroup<any>({
-    date: new FormControl(''),
-    time: new FormControl(''),
+  pickerDate = new FormControl<NgbDate>(
+    new NgbDate(this.date.year(), this.date.month(), this.date.day())
+  );
+  pickerTime = new FormControl<NgbTimeStruct>({
+    hour: this.date.hour(),
+    minute: this.date.minute(),
+    second: 0,
   });
+
+  isPopupVisible = false;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   setDate() {
-    const date = this.form.value.date;
-    const time = this.form.value.time;
+    const date = this.pickerDate.value!;
+    const time = this.pickerTime.value!;
     this.date = moment(
       `${date.year}-${date.month}-${date.day} ${time.hour}:${time.minute}`,
       'yyyy-M-D H:m'
     );
 
     this.dateChange.emit(this.date);
+  }
+
+  toggleCalendarPopup() {
+    this.isPopupVisible = !this.isPopupVisible;
   }
 }
