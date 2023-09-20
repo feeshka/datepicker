acedf6317ef48ba437b9dd0e0f334fd5602450da
MilaMichailova <kosarevitch.mila2010@gmail.com>
2023-09-20 21:39:39 +0300

    Корректный парсинг даты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { Moment } from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ui-bootstrap';
+
+  testDate: Moment = moment();
+
+  public get datetime(): Moment {
+    return this.testDate;
+  }
 }
--- src/app/components/date-time-picker/date-time-picker.component.ts
+++ src/app/components/date-time-picker/date-time-picker.component.ts
@@ -6,29 +6,29 @@ import { Moment } from 'moment';
 @Component({
   selector: 'app-date-time-picker',
   templateUrl: './date-time-picker.component.html',
-  styleUrls: ['./date-time-picker.component.scss']
+  styleUrls: ['./date-time-picker.component.scss'],
 })
 export class DateTimePickerComponent implements OnInit {
-
   @Input() date: Moment = moment();
   @Output() dateChange: EventEmitter<Moment> = new EventEmitter<Moment>();
 
   form = new FormGroup<any>({
     date: new FormControl(''),
     time: new FormControl(''),
-  })
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   setDate() {
-    console.log('date', this.form.value.date);
-    console.log('time', this.form.value.time);
     const date = this.form.value.date;
     const time = this.form.value.time;
-    this.date = moment(`${date.month}`, "MM-DD-YYYY");;
-  }
+    this.date = moment(
+      `${date.year}-${date.month}-${date.day} ${time.hour}:${time.minute}`,
+      'yyyy-M-D H:m'
+    );
 
+    this.dateChange.emit(this.date);
+  }
 }
